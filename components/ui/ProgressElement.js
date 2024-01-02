567391643941318025c635620331dd4b20d0a950
beargreenholtz <beargreenholtz@gmail.com>
2024-01-02 14:47:11 +0200

    fix days

--- components/ui/ProgressElement.js
+++ components/ui/ProgressElement.js
@@ -6,16 +6,12 @@ import * as Animatable from 'react-native-animatable';
 import { calculateDaysPassed, calculateDaysPassedToTarget } from '../../utils/days-caculator';
 
 function ProgressElement(props) {
-	let currentTimestamp = Date.now();
-	let threeDaysAgoTimestamp = currentTimestamp - 1 * 24 * 60 * 60 * 1000;
-	let threeDaysAgo = new Date(threeDaysAgoTimestamp);
-
 	const timeReached = new Date(props.targetDate) - Date.now() <= 0;
 
 	return (
 		<Animatable.View animation="fadeInUp" style={props.style}>
 			<CircularProgress
-				value={timeReached ? 1 : calculateDaysPassed(threeDaysAgo)}
+				value={timeReached ? 1 : calculateDaysPassed(props.createdDate)}
 				radius={props.isSmall ? 45 : 80}
 				duration={100}
 				progressValueColor={'black'}
